a1e88befb9537c65f7e9887c6369f6506e0a97c8
edson bruno <sucloudflare.ddos21@gmail.com>
2024-11-01 15:40:26 -0300

    Mensagem do commit

--- public/middleware.js
+++ public/middleware.js
@@ -5,7 +5,5 @@ export const config = { matcher: '/welcome' };
 
 export async function middleware() {
   const greeting = await get('greeting');
-  // NextResponse.json requires at least Next v13.1 or
-  // enabling experimental.allowMiddlewareResponseBody in next.config.js
   return NextResponse.json(greeting);
 }
